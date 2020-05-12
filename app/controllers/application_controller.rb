1e0b2dfb57fa7fa0cf9a95564116bf126d2b5054
agdsarge <agdsargeant@gmail.com>
2020-05-12 17:00:06 -0400

    fixed new.erb typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,45 +3,45 @@ require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+    end
 
-  get '/' do
-   "Welcome!"
-  end
+    get '/' do
+        erb :homepage
+    end
 
-  get '/articles' do 
-    @articles = Article.all
-    erb :index
-  end 
+    get '/articles' do
+        @articles = Article.all
+        erb :index
+    end
 
-  get '/articles/new' do
-    erb :new
-  end 
+    get '/articles/new' do
+        erb :new
+    end
 
-  get '/articles/:id' do 
-    article_id = params[:id]
-    @article = Article.find(article_id)
-    erb :show
-  end 
+    get '/articles/:id' do
+        article_id = params[:id]
+        @article = Article.find(article_id)
+        erb :show
+    end
 
-  post '/articles' do 
-    title = params[:title]
-    content = params[:content]
-    article = Article.find_or_create_by(
-      title: title,
-      content: content)
-    redirect "/articles/#{article.id}"
-  end 
+    post '/articles' do
+        title = params[:title]
+        content = params[:content]
+        article = Article.find_or_create_by(
+            title: title,
+            content: content)
+            redirect "/articles/#{article.id}"
+        end
 
-  delete '/articles/:id' do
-    Article.delete(params[:id])
-    redirect '/articles'
-  end
+    delete '/articles/:id' do
+        Article.delete(params[:id])
+        redirect '/articles'
+    end
 
 
 
-  
-end
+
+    end
